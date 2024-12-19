de8769da5e5e18d8cb7324237520b8fb8e961781
Rockybhai21 <rockybhai2109rs@gmail.com>
2024-12-19 21:12:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -14,8 +14,8 @@ class script(object):
     CAPTION = """<b>🎀 Title : @Drama_Loverx {file_name}
 
 sɪᴢᴇ 📥: {file_size}
-➥ 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 :- {file_quality}
-➥ 𝐅𝐢𝐥𝐞 𝐒𝐢𝐳𝐞:-  {file_size}
+➥ 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 :- {quality}
+➥ 𝐅𝐢𝐥𝐞 𝐒𝐢𝐳𝐞:- {file_size}
 
 <blockquote>@Real_Pirates</blockquote>
 
